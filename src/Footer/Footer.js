a98184dcdf0f7e1ec5f34506dc3584998cf73264
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2021-04-19 13:22:12 +0300

    changed

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+
+
+
+class Foot  extends React.Component{
+    render() {
+        return (
+             <div className="foot">
+                 hello there
+             </div>
+        );
+    }
+}
+
+export default Foot;
--- src/Saying/Saying.js
+++ src/Saying/Saying.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Dot from './dot.jpg';
 
 
 class Say extends React.Component{
     render() {
         return (
-             <div>
-hello
+             <div className="say">
+                 <img className="dots" src={Dot} alt=""/>
+                 <p className="p">"Act as if what you do makes a difference. It does"</p>
+                 <p className="par">William James</p>
              </div>
         );
     }
